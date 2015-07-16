bcb770b7fc59340d3816d63c1d1b89756be7759a
helmy-io <helmy@tutanota.com>
2015-07-17 01:15:18 +0200

    Edited author name

--- simple-chroot.sh
+++ simple-chroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2015, M. Helmy Hemida. All rights reserved.
+# Copyright (c) 2015, M. Helmy Hemeda. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
